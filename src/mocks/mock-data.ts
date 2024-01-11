4cfe022531f20db4e85c47e1e98b64d4faff1453
davib <davi.brantes@gmail.com>
2024-01-11 19:35:03 -0300

    feat: add novos dados

--- src/mocks/mock-data.ts
+++ src/mocks/mock-data.ts
@@ -7,43 +7,377 @@ export const ALBUMS: Album[] = [
     artists: "Olivia Rodrigo",
     musics: [
         {
+            title: "all-american bitch",
+            artists: "Olivia Rodrigo",
+            release: new Date(),
+            duration: 165
+        },
+        {
+            title: "bad idea right?",
+            artists: "Olivia Rodrigo",
+            release: new Date(),
+            duration: 184
+        },
+        {
             title: "vampire",
             artists: "Olivia Rodrigo",
             release: new Date(),
             duration: 219
         },
         {
-            title: "vampire",
+            title: "lacy",
             artists: "Olivia Rodrigo",
             release: new Date(),
-            duration: 7190
+            duration: 177
         },
+        {
+            title: "ballad of a homeschooled girl",
+            artists: "Olivia Rodrigo",
+            release: new Date(),
+            duration: 203
+        },
+        {
+            title: "making the bed",
+            artists: "Olivia Rodrigo",
+            release: new Date(),
+            duration: 198
+        },
+        {
+            title: "logical",
+            artists: "Olivia Rodrigo",
+            release: new Date(),
+            duration: 231
+        },
+        {
+            title: "get him back!",
+            artists: "Olivia Rodrigo",
+            release: new Date(),
+            duration: 211
+        },
+        {
+            title: "love is embarassing",
+            artists: "Olivia Rodrigo",
+            release: new Date(),
+            duration: 154
+        },
+        {
+            title: "the grudge",
+            artists: "Olivia Rodrigo",
+            release: new Date(),
+            duration: 189
+        },
+        {
+            title: "pretty isn't pretty",
+            artists: "Olivia Rodrigo",
+            release: new Date(),
+            duration: 199
+        },
+        {
+            title: "teenage dream",
+            artists: "Olivia Rodrigo",
+            release: new Date(),
+            duration: 222
+        },
+        
+        
+        
     ],
-    cover: "https://m.media-amazon.com/images/I/91rzIsvpwjL._AC_UF1000,1000_QL80_.jpg",
+    cover: "https://i.scdn.co/image/ab67616d0000b273e85259a1cae29a8d91f2093d",
     release: new Date()
   },
   {
     title: "UTOPIA",
     artists: "Travis Scott",
-    musics: [{
-      title: "MY EYES",
-      artists: "Travis Scott",
-      release: new Date(),
-      duration: 73
-    }],
-    cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQMsEU2qPSRc2UzmC_sjJS2acb21yPNBoYaDNDr7RyQ6qG62fFO8R3nPS5oQYmr2p6eT4&usqp=CAU",
+    musics: [
+        {
+            title: "HYAENA",
+            artists: "Travis Scott",
+            release: new Date(),
+            duration: 222
+        },        
+        {
+            title: "THANK GOD",
+            artists: "Travis Scott",
+            release: new Date(),
+            duration: 184
+        },        
+        {
+            title: "MODERN JAM (feat. Teezo Touchdown)",
+            artists: "Travis Scott, Teezo Touchdown",
+            release: new Date(),
+            duration: 255
+        },
+        {
+            title: "MY EYES",
+            artists: "Travis Scott",
+            release: new Date(),
+            duration: 251
+        },
+        {
+            title: "GOD'S COUNTRY",
+            artists: "Travis Scott",
+            release: new Date(),
+            duration: 127
+        },
+        {
+            title: "SIRENS",
+            artists: "Travis Scott",
+            release: new Date(),
+            duration: 204
+        },
+        {
+            title: "Meltdown (feat. Drake)",
+            artists: "Travis Scott, Drake",
+            release: new Date(),
+            duration: 246
+        },   
+        {
+            title: "FEIN (feat. Playboi Carti)",
+            artists: "Travis Scott, Playboi Carti",
+            release: new Date(),
+            duration: 191
+        },
+        {
+            title: "DELRESTO (ECHOES) (feat. Beyoncé)",
+            artists: "Travis Scott, Beyoncé",
+            release: new Date(),
+            duration: 274
+        },
+        {
+            title: "I KNOW?",
+            artists: "Travis Scott",
+            release: new Date(),
+            duration: 211
+        },
+        {
+            title: "TOPIA TWINS (feat. Rob49 & 21 Savage)",
+            artists: "Travis Scott, Rob49, 21 Savage",
+            release: new Date(),
+            duration: 223
+        },
+        {
+            title: "CIRCUS MAXIMUS (feat. The Weeknd & Swae Lee)",
+            artists: "Travis Scott, The Weeknd, Swae Lee",
+            release: new Date(),
+            duration: 258
+        },
+        {
+            title: "PARASAIL (feat. Yung Lean & Dave Chappelle)",
+            artists: "Travis Scott, Yung Lean, Dave Chappelle",
+            release: new Date(),
+            duration: 154
+        },
+        {
+            title: "SKITZO (feat. Yung Thug)",
+            artists: "Travis Scott, Yung Thug",
+            release: new Date(),
+            duration: 336
+        },
+        {
+            title: "LOST FOREVER (feat. Westside Gunn)",
+            artists: "Travis Scott, Westside Gunn",
+            release: new Date(),
+            duration: 163
+        },
+        {
+            title: "LOOOVER (feat. Kid Cudi)",
+            artists: "Travis Scott, Kid Cudi",
+            release: new Date(),
+            duration: 226
+        },
+        {
+            title: "K-POP (feat. Bad Bunne & The Weeknd)",
+            artists: "Travis Scott, Bad Bunne, The Weeknd",
+            release: new Date(),
+            duration: 185
+        },
+        {
+            title: "TELEKINESIS (feat. SZA & Future)",
+            artists: "Travis Scott, SZA, Future",
+            release: new Date(),
+            duration: 246
+        },
+        {
+            title: "TIL FURTHER NOTICE (feat. James Blake & 21 Savage)",
+            artists: "Travis Scott, James Blake, 21 Savage",
+            release: new Date(),
+            duration: 353
+        },
+    ],
+    cover: "https://i.scdn.co/image/ab67616d0000b273881d8d8378cd01099babcd44",
     release: new Date()
   },
   {
     title: "Flower Boy",
     artists: "Tyler, The Creator",
-    musics: [{
-      title: "MY EYES",
-      artists: "Travis Scott",
-      release: new Date(),
-      duration: 73
-    }],
-    cover: "https://www.aimm.edu/hubfs/Blog%20Images/Top%2010%20Album%20Covers%20of%202017/Tyler%20the%20Creator-%20Flower%20boy.jpg#keepProtocol",
+    musics: [
+        {
+            title: "Foreword (feat. Rex Orange County)",
+            artists: "Tyler, The Creator, Rex Orange County",
+            release: new Date(),
+            duration: 194
+        },
+        {
+            title: "where This Flower Blooms (feat. Frank Ocean)",
+            artists: "Tyler, The Creator, Frank Ocean",
+            release: new Date(),
+            duration: 194
+        },
+        {
+            title: "Sometimes...",
+            artists: "Tyler, The Creator",
+            release: new Date(),
+            duration: 36
+        },
+        {
+            title: "See You Again (feat. Kali Uchis)",
+            artists: "Tyler, The Creator, Kali Uchis",
+            release: new Date(),
+            duration: 180
+        },
+        {
+            title: "Who Dat Boy (feat. A$AP Rocky)",
+            artists: "Tyler, The Creator, A$AP Rocky",
+            release: new Date(),
+            duration: 205
+        },
+        {
+            title: "Pothole (feat. Jade Smith)",
+            artists: "Tyler, The Creator, Jade Smith",
+            release: new Date(),
+            duration: 237
+        },
+        {
+            title: "Garden Shed (feat. Estelle)",
+            artists: "Tyler, The Creator, Estelle",
+            release: new Date(),
+            duration: 223
+        },
+        {
+            title: "Boredom (feat. Rex Orange County & Anna of the North)",
+            artists: "Tyler, The Creator, Rex Orange County, Anna of the North",
+            release: new Date(),
+            duration: 320
+        },
+        {
+            title: "I Ain't Got Time!",
+            artists: "Tyler, The Creator",
+            release: new Date(),
+            duration: 206
+        },
+        {
+            title: "911 / Mr. Lonely (feat. Frank Ocean & Steve Lacy)",
+            artists: "Tyler, The Creator, Frank Ocean, Steve Lacy",
+            release: new Date(),
+            duration: 255
+        },
+        {
+            title: "911 / Mr. Lonely (feat. Lil'Wayne)",
+            artists: "Tyler, The Creator, Lil'Wayne",
+            release: new Date(),
+            duration: 60
+        },
+        {
+            title: "November",
+            artists: "Tyler, The Creator",
+            release: new Date(),
+            duration: 225
+        },
+        {
+            title: "Glitter",
+            artists: "Tyler, The Creator",
+            release: new Date(),
+            duration: 224
+        },
+        {
+            title: "Enjoy Right Now, Today",
+            artists: "Tyler, The Creator",
+            release: new Date(),
+            duration: 235
+        },
+    ],
+    cover: "https://i.scdn.co/image/ab67616d0000b2738940ac99f49e44f59e6f7fb3",
     release: new Date()
   },
+  {
+    title: "Nada Como um Dia Após o Outro Dia Vol. 1",
+    artists: "Racionais MC's",
+    musics: [
+        {
+            title: "Sou + Você",
+            artists: "Racionais MC's",
+            release: new Date(),
+            duration: 108
+        },
+        {
+            title: "Vivão e Vivendo",
+            artists: "Racionais MC's",
+            release: new Date(),
+            duration: 117
+        },
+        {
+            title: "Vida Loka (Intro)",
+            artists: "Racionais MC's",
+            release: new Date(),
+            duration: 24
+        },
+        {
+            title: "Vida Loka, Pt. 1",
+            artists: "Racionais MC's",
+            release: new Date(),
+            duration: 303
+        },
+        {
+            title: "Nego Drama",
+            artists: "Racionais MC's",
+            release: new Date(),
+            duration: 411
+        },
+    ],
+    cover: "https://i.scdn.co/image/ab67616d0000b273497e68476e7062b1bf5ea83f",
+    release: new Date()
+  },
+  {
+    title: "Essencial - Ao Vivo",
+    artists: "Rosa de Saron",
+    musics: [
+        {
+            title: "Folhas do Chão - Ao Vivo",
+            artists: "Rosa de Saron",
+            release: new Date(),
+            duration: 147
+        }
+    ],
+    cover: "https://i.scdn.co/image/ab67616d0000b2730580feca875bc7ebe0d8b647",    
+    release: new Date()
+  },
+  {
+    title: "Sonhar e Viver",
+    artists: "Salzband",
+    musics: [
+        {
+            title: "O Peso Não É Só Meu",
+            artists: "Salzband",
+            release: new Date(),
+            duration: 244
+        }
+    ],
+    cover: "https://i.scdn.co/image/ab67616d0000b273c57ad66d2f7602ab95221ec0",    
+    release: new Date()
+  },
+  {
+    title: "Take Two",
+    artists: "BTS",
+    musics: [
+        {
+            title: "Take Two",
+            artists: "BTS",
+            release: new Date(),
+            duration: 229
+        }
+    ],
+    cover: "https://i.scdn.co/image/ab67616d0000b2738a701e76e8845928f6cd81c8",    
+    release: new Date()
+  }
+  
 ];
